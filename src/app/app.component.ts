83063b084f6ffbe51f7d9987fbf0a9c68b5779ec
dudulive <edurodriguesbraz@hotmail.com>
2018-10-08 21:49:57 -0300

    Tipando nossa API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { PhotoService } from './photos/photo/photo.service';
 import { Component } from '@angular/core';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Alurapic';
-  photos: Object[] = [];
+  photos: Photo[] = [];
 
   constructor(photoService: PhotoService) {
     photoService.listFromUser('flavio')
