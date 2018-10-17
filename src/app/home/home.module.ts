7f71e3a40ccd5b08db6e40adfcfae05809edfed7
dudulive <edurodriguesbraz@hotmail.com>
2018-10-16 22:48:28 -0300

    O componente de login

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { SignInComponent } from "./signin/signin.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations: [ SignInComponent ],
+    imports: [ CommonModule ]
+})
+export class HomeModule {
+
+}
