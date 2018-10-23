f4a873ba008e7e801f9059c12e6b2fa9dbd55b5e
dudulive <edurodriguesbraz@hotmail.com>
2018-10-22 21:32:17 -0300

    Cabeçalho da aplicação

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./header/header/header.component";
+
+@NgModule({
+    declarations: [HeaderComponent],
+    exports: [HeaderComponent],
+    imports: [ CommonModule ]
+})
+export class CoreModule {
+
+}
